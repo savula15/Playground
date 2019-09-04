b1bf117281b6db2da98a19bc12552bcf6d46b12c
savula15 <savula15@gmail.com>
2019-09-04 12:31:59 -0700

    bowling Game score

--- programs/bowlingGame.py
+++ programs/bowlingGame.py
@@ -6,7 +6,7 @@ def bowlingGame(a):
 		if a[index] == 10:
 			asum += 10 + a[index+1]
 			if asum == 20:
-				asum += a[index+1]
+				asum += a[index+2]
 			frames.append(asum)
 			index += 1
 		else:
@@ -16,3 +16,7 @@ def bowlingGame(a):
 			frames.append(asum)
 			index += 2
 	return sum(frames)
+
+
+assert bowlingGame([1, 3, 10, 10, 10, 6, 4, 5, 1, 4, 5, 8, 10]) == 124
+assert bowlingGame([1, 3, 10, 6, 4, 5, 1, 4, 5, 8]) == 58
